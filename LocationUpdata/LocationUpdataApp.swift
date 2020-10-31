b1f952721b2545820f0be7f3a5de3cf65f7b6866
hiroshi yamato <yamato@gmail.com>
2020-11-01 07:26:11 +0900

    Initial Commit

---
+++ LocationUpdata/LocationUpdataApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationUpdataApp.swift
+//  LocationUpdata
+//
+//  Created by hiroshi yamato on 2020/11/01.
+//
+
+import SwiftUI
+
+@main
+struct LocationUpdataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
